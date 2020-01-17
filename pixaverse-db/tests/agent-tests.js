8c7b9eecf70484eec31fef798dab9c13bd36013b
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-01-17 18:17:06 -0500

    Ava, AgentTest, Testing connection with db and behavior the api

--- pixaverse-db/tests/agent-tests.js
+++ pixaverse-db/tests/agent-tests.js
@@ -1,17 +1,46 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 const config = {
   logging: function () {}
 }
+
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub =null
 let db = null
+let sandbox = null
 // antes de cada uno de los test ejecute la siguiente función asyncrona
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../',{
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
+})
+
+test.afterEach(() =>{
+  sandbox && sinon.resetHistory()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist') // resuelve a verdadero, no necesariamente tiene que se verdadero
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument needs to be the AgentModel')
 })
